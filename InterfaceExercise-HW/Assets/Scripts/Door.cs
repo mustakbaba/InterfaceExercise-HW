857a950704154b2aaff49854f3e3879833f5fc6a
IdrisDindar <idrisdindar@outlook.com>
2020-12-19 00:01:21 +0300

    Revert "Revert "Interface""
    This reverts commit eaafdb4e15a8ce002a8aaba9f77c08039ec6b849.

---
+++ InterfaceExercise-HW/Assets/Scripts/Door.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : UsableBase
+{
+    public Door()
+    {
+        message = "Aç";
+    }
+    public override void Use()
+    {
+        Debug.Log("Kapı açıldı");
+    }
+}
---
+++ InterfaceExercise-HW/Assets/Scripts/UsableBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class UsableBase : MonoBehaviour, IUsable
+{
+    public string message;
+
+    public virtual void Collect()
+    {
+        Debug.Log(message);
+    }
+    public abstract void Use();
+}
