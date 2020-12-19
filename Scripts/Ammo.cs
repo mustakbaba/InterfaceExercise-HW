4fbdbe3bd6f0c9b56103eb8461a66267faa8589b
Cemre Naz Yasar <73537562+cemrenazyasar@users.noreply.github.com>
2020-12-19 13:53:38 +0300

    Add files via upload

---
+++ Scripts/Ammo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour, IInteractable
+{
+    public string InfoMessage()
+    {
+        return "COLLECT THE AMMEO";
+    }
+
+    public void Interact()
+    {
+        Collect();
+    }
+
+    private void Collect()
+    {
+        Debug.Log("Ammo collected!");
+        Destroy(gameObject);
+    }
+}
